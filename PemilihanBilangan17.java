8c3e0da5f15f81faf535e566ed33d758ac1cee24
zaki setyawan <kiminsetyawan@gmail.com>
2024-09-28 17:17:46 +0700

    Sesudah diupdate menjadi tenary

--- PemilihanBilangan17.java
+++ PemilihanBilangan17.java
@@ -1,18 +1,17 @@
 import java.util.Scanner;
 
-public class PemilihanBilangan17{
+public class PemilihanBilangan17 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Masukkan sebuah angka: ");
         int angka = sc.nextInt();
-        
-        if (angka % 2 == 0) {
-             System.out.println("Angka " + angka + " termasuk bilangan genap.");
-        }
-        else {
-        System.out.println("Angka " + angka + " termasuk bilangan genap.");
-        }
+
+        // Menggunakan ternary operator untuk menentukan apakah angka genap atau ganjil
+        String hasil = (angka % 2 == 0) ? "Angka " + angka + " termasuk bilangan genap."
+                                        : "Angka " + angka + " termasuk bilangan ganjil.";
+
+        System.out.println(hasil);
     }
 }
